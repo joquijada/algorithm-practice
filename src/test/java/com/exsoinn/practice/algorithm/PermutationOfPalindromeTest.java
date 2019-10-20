8a81ba326b4f3f50aa0660c9558f862ff1835b42
Jose Quijada <jose.quijada@one.verizon.com>
2019-10-20 17:50:44 -0400

    feat(alg): latest

--- src/test/java/com/exsoinn/practice/algorithm/PermutationOfPalindromeTest.java
+++ src/test/java/com/exsoinn/practice/algorithm/PermutationOfPalindromeTest.java
@@ -6,14 +6,26 @@ import junit.framework.TestCase;
  * @author josequijada
  */
 public class PermutationOfPalindromeTest extends TestCase {
+  private static final String permPalin = "Tact Coa";
+  private static final String notPermPalin = "Tact Co";
 
   private static final PermutationOfPalindrome permutationOfPalindrome = new PermutationOfPalindrome();
 
   public void testIsPalindromePermutationHorrendous() {
-    assertTrue(permutationOfPalindrome.isPalindromePermutationHorrendous("Tact Coa"));
+    assertTrue(permutationOfPalindrome.isPalindromePermutationHorrendous(permPalin));
   }
 
   public void testIsPalindromePermutationImproved() {
+    assertTrue(permutationOfPalindrome.isPalindromePermutationHorrendous(permPalin));
+  }
+
+
+  public void testIsNotPalindromePermutationHorrendous() {
+    assertTrue(!permutationOfPalindrome.isPalindromePermutationHorrendous(notPermPalin));
+  }
+
+  public void testIsNotPalindromePermutationImproved() {
+    assertTrue(!permutationOfPalindrome.isPalindromePermutationHorrendous(notPermPalin));
   }
 
 }
